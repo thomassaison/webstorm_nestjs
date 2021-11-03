5ac9df040eeddcd68bb1b55e4eb696b8d6a662ad
GitHub Action <action@github.com>
2021-11-03 17:17:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/thomassaison/webstormnestjs/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.thomassaison.webstormnestjs.services
+
+import com.github.thomassaison.webstormnestjs.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
